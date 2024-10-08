c1088567c71bf8ac4f35f4a8fb8b1632d12e2985
newgamer11 <qfpotw4u81@naver.com>
2024-10-08 16:31:47 +0900

    fourth

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "Hello, Maple World!";
+        return "Hello, tiger World!";
     }
 }
